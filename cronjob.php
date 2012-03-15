8441e3f732738f50895bb9a9640edb95d415c813
Leon Juffermans <leon@Leon-MBP.local>
2012-03-15 21:21:44 +0100

    Security fix: Preventing direct access to 'cronjob.php' (changed method)

--- cronjob.php
+++ cronjob.php
@@ -1,5 +1,7 @@
 #!/usr/bin/php
 <?php
+if (PHP_SAPI == "cli")
+{
 	include "settings.inc.php";
 	include "request.class.php";
 	include "database.class.php";
@@ -11,4 +13,9 @@
 	$values = '"'. implode('","', $data['val']) .'"';	
 	
 	$db->addHourlyData($data['tm'], $data['un'], $data['dt'], $values);
+}
+else
+{
+	echo "No direct access allowed!";
+}
 ?>
